c75e2c5a158dac3ec393b85ae9f8c70f181ee38e
mmalia <mike_spriggan@hotmail.com>
2017-01-28 13:28:11 +0100

    login

--- login.php
+++ login.php
@@ -25,7 +25,40 @@
               printf("Connection failed: %s\n", $connection->connect_error);
               exit();
           }
-  ?>
-  </div>
+          //Validacion de la base de datos, en caso de error que lo muestre.
+          //MAKING A SELECT QUERY
+          //Password coded with md5 at the database. Look for better options
+          $consulta="select * from usuarios where
+          email='".$_POST["user"]."' and clave=md5('".$_POST["password"]."');";
+          //Test if the query was correct
+          //SQL Injection Possible
+          //Check http://php.net/manual/es/mysqli.prepare.php for more security
+          if ($result = $connection->query($consulta)) {
+              //No rows returned
+              if ($result->num_rows===0) {
+                echo "LOGIN INVALIDO";
+              } else {
+                //VALID LOGIN. SETTING SESSION VARS
+                $_SESSION["user"]=$_POST["user"];
+                $_SESSION["language"]="es";
+                header("Location: index.php");
+              }
+          } else {
+            echo "Wrong Query";
+          }
+
+      }
+    ?>
+
+    <form action="login.php" method="post">
+
+      <p><input name="user" required></p>
+      <p><input name="password" type="password" required></p>
+      <p><input type="submit" value="Log In" name="login"></p>
+
+    </form>
+</div>
+
+
   </body>
 </html>
